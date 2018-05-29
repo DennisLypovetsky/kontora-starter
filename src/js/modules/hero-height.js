6472869a65b5ad931a82701f56068dbe0348abff
Dennis Lypovetsky <tsssssssss@gmail.com>
2018-05-29 15:05:05 +0300

    Добавляет модуль для замены css: height: 100vh

--- src/js/modules/hero-height.js
+++ src/js/modules/hero-height.js
@@ -1,2 +1,3 @@
+// Правит не работающую как надо формулу CSS height: 100vh
 const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 document.getElementsByClassName('YOURCLASS')[0].style['min-height'] = height + 'px'
