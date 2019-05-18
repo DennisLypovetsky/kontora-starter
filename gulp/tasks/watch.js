8196b0adca2d595ad0eab1e4290304694d39ed91
Dennis Lypovetsky <tsssssssss@gmail.com>
2019-05-18 18:42:41 +0300

    Правит таск watch.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ module.exports = function () {
     $.gulp.watch('src/markup/**/*.pug', $.gulp.series('pug:dev'));
     $.gulp.watch('src/styles/**/*.sass', $.gulp.series('sass:dev'));
     $.gulp.watch('src/js/main.js', $.gulp.series('js:dev'));
-    $.gulp.watch('src/static/img/svg/*.svg', $.gulp.series('svg'));
+    $.gulp.watch('src/static/img/svg/*.svg', $.gulp.series(['svg:copy', 'svg:sprite']));
     $.gulp.watch('src/static/img/**/*', $.gulp.series('img:dev'));
   });
 }
